348b917fd09d8dd7715f581391ab5fba495eb2da
MSI\Anoeska Clement <a.c.lement6@gmail.com>
2018-01-17 11:07:47 +0100

    lingo

--- game.js
+++ game.js
@@ -1,1 +1,60 @@
+var random = Math.floor( Math.random() * 479);
 
+console.log(random);
+
+var answer = words[random];
+
+var answerArray = answer.split('');
+
+console.log(answer);
+
+var table = document.getElementById('table');
+
+var checkBtn = document.getElementById('check');
+
+checkBtn.setAttribute("onclick", "check()");
+
+var firstLetter = document.getElementById('letter_0');
+
+firstLetter.value = answerArray[0];
+var attempt = 1
+
+function check() {
+
+  var tr = document.createElement("tr");
+    for(i = 0;i <=4;i++){
+      var td = document.createElement("td");
+      var input = document.getElementById("letter_" + i);
+      var tekst = document.createTextNode(input.value);
+      td.style.backgroundColor = "#0685ff";
+      td.appendChild(tekst);
+      tr.appendChild(td);
+    for(j = 0;j<=4;j++){
+      if(input.value === answerArray[j]){
+        td.style.backgroundColor = "yellow";
+      }
+    }
+      if(input.value === answerArray[i]){
+        td.style.backgroundColor = "red";
+    }
+  }
+   table.appendChild(tr);
+
+   if(attempt === 6){
+     document.getElementById('gameover').style.display = "block"
+   }
+   else{attempt++}
+ }
+
+//automatisch naar volgende input
+$('.rij1').keydown(function (event){
+  var inputValue = event.charCode
+  if (event.which == 8 || inputValue == 46){
+      $(this).val('')
+      $(this).prev('.rij1').focus();
+      event.preventDefault()
+    }
+  else if(this.value.length == this.maxLength){
+    $(this).next('.rij1').focus();
+  }
+})
